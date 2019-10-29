d37f8a5e3f9c80ed81df24564a0b2e02615898b8
Edd571 <56877017+Edd571@users.noreply.github.com>
2019-10-29 14:14:14 +0000

    Add files via upload
    Task 1: Subtasks 3 and 4 completed, tested & commented.

--- Project.py
+++ Project.py
@@ -2,34 +2,42 @@
 #Timothy Richards, Edward Pidd, Chloe Dooley, Nathan Haigh
 #Date Started 29/10/2019
 
+# Imports
 import tkinter
 
 from tkinter import *
 from tkinter.ttk import *
 from time import strftime
-
+# Instantiate window
 window = tkinter.Tk()
-# function used to create window and then rename
 
+# Function used to create window and then rename
 window.title("GUI")
 window.minsize(160, 240)
 
-
-
-
-# pack is used to show the object in the window    
+# Pack is used to show the object in the window    
 label = tkinter.Label(window, text = "Sports Circuit App").pack()
-# function return choice prints out the radio button choice on click
+
+# Function ReturnChoice() prints out the radio button choice when it is clicked -- series[seriesChoice.get()] will get the relevant radio button choice from the array
 def ReturnChoice():
-    print("Your choice is " + str(seriesChoice.get()) + '\n'); 
-# code below instantiates radio buttons and ties variable seriesChoice to each button
+    print("Your choice is " + series[seriesChoice.get()] + '\n' ); 
+    
+# Code below instantiates radio buttons and ties variable seriesChoice to each button
 seriesChoice = tkinter.IntVar()
-tkinter.Radiobutton(window,text="Rally",padx = 20,variable=seriesChoice,value=1,command=ReturnChoice).pack(anchor=tkinter.W)
-tkinter.Radiobutton(window,text="Formula 1",padx = 20,variable=seriesChoice,value=2,command=ReturnChoice).pack(anchor=tkinter.W)
+tkinter.Radiobutton(window,text="Rally",padx = 20,variable=seriesChoice,value=0,command=ReturnChoice).pack(anchor=tkinter.W)
+tkinter.Radiobutton(window,text="Formula 1",padx = 20,variable=seriesChoice,value=1,command=ReturnChoice).pack(anchor=tkinter.W)
+
+# Define series data array
 series = ["Rally", "Formula 1"]
 
 
-seriesChoice = input("Please enter the series you wish to view information on: ")
 
 
+
+
+
+
+
+
+# This generates the window - keep at bottom of code
 window.mainloop()
