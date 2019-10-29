8e3733d81542302519d740ba8001d06c0991a6dd
timr12299 <56877012+timr12299@users.noreply.github.com>
2019-10-29 14:56:02 +0000

    Add files via upload

--- Project.py
+++ Project.py
@@ -4,6 +4,7 @@
 
 # Imports
 import tkinter
+import datetime
 
 from tkinter import *
 from tkinter.ttk import *
@@ -13,7 +14,7 @@ window = tkinter.Tk()
 
 # Function used to create window and then rename
 window.title("GUI")
-window.minsize(160, 240)
+window.minsize(300, 400)
 
 # Pack is used to show the object in the window    
 label = tkinter.Label(window, text = "Sports Circuit App").pack()
@@ -21,22 +22,22 @@ label = tkinter.Label(window, text = "Sports Circuit App").pack()
 # Function ReturnChoice() prints out the radio button choice when it is clicked -- series[seriesChoice.get()] will get the relevant radio button choice from the array
 def ReturnChoice():
     print("Your choice is " + series[seriesChoice.get()] + '\n' ); 
+    if(series[seriesChoice.get()] == series[1]):
+        print (f1Racesinfo[0][0] +" on the " + f1Racesinfo [1][0]) 
     
 # Code below instantiates radio buttons and ties variable seriesChoice to each button
 seriesChoice = tkinter.IntVar()
 tkinter.Radiobutton(window,text="Rally",padx = 20,variable=seriesChoice,value=0,command=ReturnChoice).pack(anchor=tkinter.W)
 tkinter.Radiobutton(window,text="Formula 1",padx = 20,variable=seriesChoice,value=1,command=ReturnChoice).pack(anchor=tkinter.W)
 
+
 # Define series data array
 series = ["Rally", "Formula 1"]
+f1Racesinfo=[["Circuit of the Americas","Autódromo José Carlos Pace"],["2019,11,3","2019,11,17"]]
 
 
 
-
-
-
-
-
+    
 
 
 # This generates the window - keep at bottom of code
